1802e6dd248a18b5e85b6ae09eefc343aef939d5
Anas Khan <anas.khan96@outlook.com>
2017-02-05 01:05:14 +0530

    added Week4

---
+++ Week4/Merge Sort/mergesort.go
@@ -0,0 +1,53 @@
+/* Copyright, Anas Khan © 2017 */
+
+package main
+
+import "fmt"
+
+func main() {
+	var n, num int // size of the array
+	fmt.Scanf("%d", &n)
+	arr := make([]int, 0, 10)
+	for i := 0; i < n; i++ {
+		fmt.Scanf("%d", &num) // each element of the array
+		arr = append(arr, num)
+	}
+	sorted := merge_sort(arr)
+	fmt.Println(sorted)
+}
+
+func merge_sort(a []int) []int {
+	if len(a) < 2 {
+		return a
+	}
+	mid := len(a) / 2
+	/* Dividing */
+	x1 := merge_sort(a[:mid])
+	x2 := merge_sort(a[mid:])
+	/* Conquering and returning */
+	return merge(x1, x2)
+}
+
+func merge(x, y []int) []int {
+	z := make([]int, len(x)+len(y))
+	var i, j int
+	for i < len(x) && j < len(y) {
+		if x[i] < y[j] {
+			z[i+j] = x[i]
+			i++
+		} else {
+			z[i+j] = y[j]
+			j++
+		}
+	}
+	/* When either of the passed arrays' traversal finishes */
+	for i < len(x) {
+		z[i+j] = x[i]
+		i++
+	}
+	for j < len(y) {
+		z[i+j] = y[j]
+		j++
+	}
+	return z
+}
---
+++ Week4/Merge Sort/mergesort_test.go
@@ -0,0 +1,28 @@
+/* Copyright, Anas Khan © 2017 */
+
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+type testpair struct {
+	arr      []int
+	expected []int
+}
+
+/* Example test pairs */
+var tests = []testpair{{[]int{11, 17, 5, 3, 43, 22, 90, 65, 45, 32, 14, 89, 86, 75, 47}, []int{3, 5, 11, 14, 17, 22, 32, 43, 45, 47, 65, 75, 86, 89, 90}},
+	{[]int{123, -111, 32, 1234, 32}, []int{-111, 32, 32, 123, 1234}},
+	{[]int{45, 43, 31, 67, 87, 54, 56, 23, 90, 134, 2, 5}, []int{2, 5, 23, 31, 43, 45, 54, 56, 67, 87, 90, 134}}}
+
+/* Test Function */
+func TestMergeSort(t *testing.T) {
+	for _, pair := range tests {
+		actual := merge_sort(pair.arr)
+		if !reflect.DeepEqual(actual, pair.expected) {
+			t.Error("For", pair.arr, "expected", pair.expected, "got", actual)
+		}
+	}
+}
