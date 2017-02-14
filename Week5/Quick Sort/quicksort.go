4839938ef8a6b3a2e183ba481ee369f7c05d004d
Anas Khan <anas.khan96@outlook.com>
2017-02-14 17:25:42 +0530

    update

---
+++ Week5/Quick Sort/quicksort.go
@@ -0,0 +1,51 @@
+/* Copyright, Anas Khan © 2017 */
+
+package main
+
+import "fmt"
+
+func main() {
+	var n, num int // size of the array
+	fmt.Scanf("%d", &n)
+	arr := make([]int, 0, 10)
+	for i := 0; i < n; i++ {
+		fmt.Scanf("%d", &num) // each element of the array
+		arr = append(arr, num)
+	}
+	sorted := quick_sort(arr)
+	fmt.Println(sorted)
+}
+
+func quick_sort(a []int) []int {
+	if len(a) > 1 {
+		s := partition(a)
+		quick_sort(a[:s])
+		quick_sort(a[s+1:])
+	}
+	return a
+}
+
+func partition(a []int) int {
+	pivot := a[0]
+	i, j := 1, len(a)-1
+	for i <= j {
+		for i <= j && a[i] <= pivot {
+			i++
+		}
+		for i <= j && a[j] >= pivot {
+			j--
+		}
+		if i < j {
+			/* Swapping when i and j come across elements
+			which belong on opposite sides */
+			temp := a[i]
+			a[i] = a[j]
+			a[j] = temp
+		}
+	}
+	/* Moving the pivot element to its right position */
+	t := a[0]
+	a[0] = a[j]
+	a[j] = t
+	return j
+}
