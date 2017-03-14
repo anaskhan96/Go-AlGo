702da1c3e062555b102207d7bc1df4974113c987
Anas Khan <anas.khan96@outlook.com>
2017-03-14 22:09:54 +0530

    added test for dfs

--- Week8/DFS/dfs.go
+++ Week8/DFS/dfs.go
@@ -5,7 +5,7 @@ package main
 import "fmt"
 
 func main() {
-	var n, components, num int // size and number of compnents
+	var n, num int // n is the size of the dimension of the matrix
 	fmt.Scanf("%d", &n)
 	visit := make([]int, 0, 10)
 	adj := make([][]int, 0, 10) // the adjacency matrix
@@ -23,16 +23,18 @@ func main() {
 	for i := 0; i < n; i++ {
 		visit = append(visit, 0)
 	}
-	// Calling dfs() on all functions
-	for i := 0; i < n; i++ {
+	fmt.Println(callDfs(adj, visit))
+}
+
+func callDfs(adj [][]int, visit []int) int {
+	components := 0
+	for i := 0; i < len(adj); i++ {
 		if visit[i] == 0 {
 			components++
 			dfs(adj, i, visit)
 		}
 	}
-	fmt.Println()
-	// Printing the number of components
-	fmt.Println(components)
+	return components
 }
 
 func dfs(a [][]int, v int, visit []int) {
---
+++ Week8/DFS/dfs_test.go
@@ -0,0 +1,28 @@
+/* Copyright, Anas Khan © 2017 */
+
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+type testpair struct {
+	visit      []int
+	adj        [][]int
+	components int
+}
+
+/* Example test pairs */
+var tests = []testpair{{[]int{0, 0, 0, 0, 0}, [][]int{[]int{0, 1, 1, 1, 0}, []int{1, 0, 0, 0, 1}, []int{1, 0, 0, 0, 1}, []int{1, 0, 0, 0, 1}, []int{0, 1, 1, 1, 0}}, 1},
+	{[]int{0, 0, 0, 0, 0}, [][]int{[]int{0, 1, 1, 1, 0}, []int{1, 0, 0, 0, 0}, []int{1, 0, 0, 0, 0}, []int{1, 0, 0, 0, 0}, []int{0, 0, 0, 0, 0}}, 2}}
+
+/* Test Function */
+func TestDfs(t *testing.T) {
+	for _, pair := range tests {
+		actual := callDfs(pair.adj, pair.visit)
+		if !reflect.DeepEqual(actual, pair.components) {
+			t.Error("For", pair.adj, "expected", pair.components, "got", actual)
+		}
+	}
+}
