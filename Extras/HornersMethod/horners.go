49c86b0c582ea90db8475034e42c4410a0910dc4
Anas Khan <anas.khan96@outlook.com>
2017-03-29 16:04:16 +0530

    added horners for power calculation

--- Extras/HornersMethod/horners.go
+++ Extras/HornersMethod/horners.go
@@ -2,7 +2,10 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 	var n, num, x int // n terms for degree n-1, x for value of x
@@ -15,12 +18,30 @@ func main() {
 	fmt.Scanf("%d", &x)
 	res := horners(arr, x)
 	fmt.Println(res)
+	// Calculate power using horners method
+	var a, p int
+	fmt.Scanf("%d", &a)
+	fmt.Scanf("%d", &p)
+	res = hornersPower(a, p) // a^p
+	fmt.Println(res)
 }
 
 func horners(a []int, x int) int {
 	result := a[0]
 	for i := 1; i < len(a); i++ {
 		result = result*x + a[i]
+	}
+	return result
+}
+
+func hornersPower(a, n int) int {
+	result := 1
+	p := strconv.FormatInt(int64(n), 2) // converting it to binary
+	for _, digit := range p {
+		result = result * result
+		if digit == '1' {
+			result = result * a
+		}
 	}
 	return result
 }
