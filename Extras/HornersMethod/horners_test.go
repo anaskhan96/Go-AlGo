d168f8701ed618a6d598dfb61dff227de8e606ea
Anas Khan <anas.khan96@outlook.com>
2017-03-29 16:10:27 +0530

    added test for horners power

--- Extras/HornersMethod/horners_test.go
+++ Extras/HornersMethod/horners_test.go
@@ -14,8 +14,15 @@ type testpair struct {
 	expected int
 }
 
+type testpairPower struct {
+	a        int
+	p        int
+	expected int
+}
+
 /* Example test pairs */
 var tests = []testpair{{4, []int{2, -6, 2, -1}, 3, 5}, {4, []int{2, 0, 3, 1}, 2, 23}}
+var testsPower = []testpairPower{{3, 4, 81}, {4, 5, 1024}, {5, 17, 762939453125}}
 
 /* Test function */
 func TestHorners(t *testing.T) {
@@ -23,6 +30,15 @@ func TestHorners(t *testing.T) {
 		actual := horners(pair.arr, pair.x)
 		if !reflect.DeepEqual(actual, pair.expected) {
 			t.Error("For", pair.arr, "expected", pair.expected, "got", actual)
+		}
+	}
+}
+
+func TestHornersPower(t *testing.T) {
+	for _, pair := range testsPower {
+		actual := hornersPower(pair.a, pair.p)
+		if !reflect.DeepEqual(actual, pair.expected) {
+			t.Error("For", pair.a, "expected", pair.expected, "got", actual)
 		}
 	}
 }
